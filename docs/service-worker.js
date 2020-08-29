357ab52181127523c8ad25d53e7ea5f232d9db5a
CharlieBolot <bolot.charlie@gmail.com>
2020-08-29 13:32:29 +0200

    test

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vue-app/precache-manifest.ac84b58559741c90fb215e94fde8f547.js"
+  "/vue-app-sandrine/precache-manifest.ac84b58559741c90fb215e94fde8f547.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "vue-app"});
+workbox.core.setCacheNameDetails({prefix: "vue-app-sandrine"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
